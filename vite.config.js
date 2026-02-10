43f8b69a8a48e403962ef905d728f2071b351785
KseniyaBabaryka-creator <kseniabobariko2005@gmail.com>
2026-02-10 15:08:05 +0100

    fix base for github pages

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/service-app/',
+  base: '/services-app/',
   plugins: [
     vue(),
     vueDevTools(),
